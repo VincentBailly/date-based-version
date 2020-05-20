4dc4fc62c69ce260798a7eea8b2d57736565d4ef
Vincent Bailly <vibailly@tuta.io>
2020-05-20 08:11:29 +0200

    make not implemented tests fail

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,29 +1,32 @@
 describe("When previous version has today's date", () => {
   it("creates a new version which is a simple bump of the third component", () => {
+    throw new Error("Not implemented");
   })
 });
 
 describe("When previous version has not today's date", () => {
   it("creates a new version which contains today's date", () => {
+    throw new Error("Not implemented");
   })
 
   it("reset the third and fourth component", () => {
+    throw new Error("Not implemented");
   })
 });
 
 describe("Creation of release branch", () => {
   it("creates a release branch with the three first components of the version", () => {
-
+    throw new Error("Not implemented");
   });
 
   it("does not happen for a patch build", () => {
-
+    throw new Error("Not implemented");
   });
 })
 
 describe("When it is a patch build", () => {
   it("creates a version which is a bump of the fourth component compared to the previous version", () => {
-
+    throw new Error("Not implemented");
   })
 
 })
