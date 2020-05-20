90ec1b1bb972baae8f7a1dbb8b5cd584e7ba5bb2
Vincent Bailly <vibailly@tuta.io>
2020-05-20 08:00:38 +0200

    implement requirements in the form of tests

---
+++ src/__tests__/index.ts
@@ -0,0 +1,29 @@
+describe("When previous version has today's date", () => {
+  it("creates a new version which is a simple bump of the third component", () => {
+  })
+});
+
+describe("When previous version has not today's date", () => {
+  it("creates a new version which contains today's date", () => {
+  })
+
+  it("reset the third and fourth component", () => {
+  })
+});
+
+describe("Creation of release branch", () => {
+  it("creates a release branch with the three first components of the version", () => {
+
+  });
+
+  it("does not happen for a patch build", () => {
+
+  });
+})
+
+describe("When it is a patch build", () => {
+  it("creates a version which is a bump of the fourth component compared to the previous version", () => {
+
+  })
+
+})
