6cc163d1fcd7b6d323654868060a95b58c68e2cd
Vincent Bailly <vibailly@tuta.io>
2020-05-20 10:12:20 +0200

    implement first test

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,9 +1,35 @@
+import { directory as tmpDir } from "tempy";
+import { commandSync } from "execa";
+import { writeFileSync } from "fs";
+import { join } from "path";
+import { setVersion, getCurrentLatestVersion } from "../index";
+
 describe("When previous version has today's date", () => {
   it("creates a new version which is a simple bump of the third component", () => {
-    throw new Error("Not implemented");
+    // Setup
+    jest.spyOn(global.Date, "now").mockImplementation(() => new Date("2020-05-10T11:01:58.135Z").valueOf());
+    const repo = tmpDir();
+    commandSync("git init", { cwd: repo });
+    writeFileSync(join(repo, "foo.txt"), "foo");
+    commandSync("git add foo.txt", { cwd: repo });
+    commandSync(`git commit -m first`, { cwd: repo });
+    commandSync(`git tag v1.20200510.2.0`, { cwd: repo });
+
+    writeFileSync(join(repo, "bar.txt"), "bar");
+    commandSync("git add bar.txt", { cwd: repo });
+    commandSync(`git commit -m second`, { cwd: repo });
+    commandSync(`git tag vincent`, { cwd: repo });
+
+    // Act
+    setVersion(repo);
+
+    // Validate
+    const currentLatestVersion = getCurrentLatestVersion(repo);
+    expect(currentLatestVersion).toBe("v1.20200510.3.0");
   })
 });
 
+/*
 describe("When previous version has not today's date", () => {
   it("creates a new version which contains today's date", () => {
     throw new Error("Not implemented");
@@ -30,3 +56,4 @@ describe("When it is a patch build", () => {
   })
 
 })
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,54 @@
-const message: string = "foo bar";
-console.log(message);
+import { commandSync } from "execa";
+
+const versionTagRegExp = /v\d+\.\d{8}\.\d+\.\d+/;
+
+type Version = {
+  p1: number;
+  p2: number;
+  p3: number;
+  p4: number;
+}
+
+function toString(version: Version): string {
+  const { p1, p2, p3, p4 } = version;
+  return  `v${p1}.${p2}.${p3}.${p4}`;
+}
+
+function bumpMinor(old: Version): Version {
+  const { p1, p2, p4 } = old;
+  const p3 = old.p3 + 1;
+
+  return { p1, p2, p3, p4 };
+}
+
+function parseVersion(versionString): Version {
+  if (!versionTagRegExp.test(versionString)) {
+    throw new Error("the version string does not match the version regexp");
+  }
+
+  const p1 = parseInt(/v(\d+)\.\d{8}\.\d+\.\d+/.exec(versionString)[1]);
+  const p2 = parseInt(/v\d+\.(\d{8})\.\d+\.\d+/.exec(versionString)[1]);
+  const p3 = parseInt(/v\d+\.\d{8}\.(\d+)\.\d+/.exec(versionString)[1]);
+  const p4 = parseInt(/v\d+\.\d{8}\.\d+\.(\d+)/.exec(versionString)[1]);
+
+  return { p1, p2, p3, p4 };
+}
+
+export function setVersion (cwd) {
+  const currentVersion = parseVersion(getCurrentLatestVersion(cwd));
+  const newVersionTag = toString(bumpMinor(currentVersion));
+  commandSync(`git tag ${newVersionTag}`, { cwd });
+}
+
+export function getCurrentLatestVersion(cwd) {
+    const gitLogOutput = commandSync("git log --tags --date-order --format='%D'", { cwd }).stdout.toString();
+    const gitLogOutputLines = gitLogOutput.split(/\r\n|\n|\r/g).filter(l => l !== "");
+    const linesWithCorrectVersionTags = gitLogOutputLines.filter(l => versionTagRegExp.test(l));
+    if (linesWithCorrectVersionTags.length === 0) {
+      throw new Error("no valid version tag found in the git history");
+    }
+
+    const currentVersionTag = versionTagRegExp.exec(linesWithCorrectVersionTags[0])[0];
+
+    return currentVersionTag;
+}
